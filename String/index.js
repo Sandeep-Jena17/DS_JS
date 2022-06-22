1c6804a7e60bef918a60fb6e4708fc1125363d0b
Sandeep <sandeep.jena@empower-solutions.co.in>
2022-06-23 00:56:37 +0530

    reversed string

--- String/index.js
+++ String/index.js
@@ -1,1 +1,25 @@
-//string ds
+//string ds 
+//String reversal
+//Write  code for reversing string 
+const str1="Sandeep";
+//Output: peednaS
+
+//Reverse a string first problem.
+const reverseString=(str)=>{
+
+    const reversed=str.split('').reverse()
+    console.log(reversed)
+};
+
+reverseString("Sandeep")
+
+const reverseStr=(str)=>{
+    let reverse='';
+    for(let a=str.length-1; a>=0; a--){
+        console.log("a")
+       reverse+=str[a]
+    }
+    console.log("jj",reverse)
+return reverse
+}
+reverseStr("ctuuui");
---
+++ javaScriptBasic.js
@@ -0,0 +1,20 @@
+/* 
+@Author:Sandeep Kumar Jena
+JS Developer and Learner 
+Building stuff using React and Node js 
+I love JS 
+*/
+
+/* 0th topic for interview */
+
+// Expression on Js
+/*  1.Function expression & function declataion
+   f
+    const postUserData=()=>{
+
+    }
+    function declarion : The function declaration (function statement) defines a function with the specified parameters.
+    function postUserData(){
+
+    }
+ */
